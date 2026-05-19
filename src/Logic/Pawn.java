516bdb19826b02812de6b27f81bdee592c990e4a
Cristian <martinez.rafael.cristian@gmail.com>
2026-05-18 23:45:09 -0600

    logica implementada para el peon

--- src/Logic/Pawn.java
+++ src/Logic/Pawn.java
@@ -31,7 +31,7 @@ public class Pawn extends Piece {
      *        <li>Que exista un movimiento (No te puedes quedar donde estas)</li>
      *        <li>Que el camino del movimiento no se encuentre obstruido por piezas propias o contrarias</li>
      *        <li>No puedes capturar piezas amigas</li>
-     *        <li>Puedes moverte en diagonal</li>
+     *        <li>Puedes capturar unicamente en diagonal</li>
      *    </ul>
      * </p>
      * @param board Tablero actual donde evitaremos las colisiones
@@ -44,10 +44,18 @@ public class Pawn extends Piece {
 
         int color = -1;
         if (isWhite) color = 1;
-
         if(newX == x && newY == y )return false;
 
+        Piece target = board.getPiece(newX, newY);
 
+        if(newX == x && newY == y+color && board.getPiece(newX,newY) == null)return true;
+
+        if (board.getPiece(newX, newY) != null && newY == (y + color) && Math.abs(newX-x) == 1 && isWhite ^ target.isWhite ){
+            return true;
+        }
+
+
+        if(isFirstMove && newY == y + color*2  && board.isPathClear(x,y,newX,newY) && target == null && newX == x)return true;
 
 
         return false;
